3a5dca8ebf863e7929b7290cce15f188bd53d493
Champa Lal suthar <135296999+champalalsuthar@users.noreply.github.com>
2024-02-10 22:12:36 +0530

    Update resume link constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/champalalsuthar",
   resume:
-    "https://drive.google.com/file/d/1Re3p5ePh9xn_ms4uh0Gi20haGMenPboy/view",
+    "https://drive.google.com/file/d/1TPnbAw6XGwg5PvBax2IZQnwS0w26UzmJ/view",
   linkedin: "https://www.linkedin.com/in/champa-lal-suthar-325040253/",
   twitter: "https://www.twitter.com/",
   insta: "https://www.instagram.com/",
